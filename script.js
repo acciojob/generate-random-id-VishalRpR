7b2fa5cd1ba7699dac3a89c79cda57501041b896
Vishal <120316470+VishalRpR@users.noreply.github.com>
2026-07-23 11:15:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function makeid(l) {
   // write your code here
+	const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let str=""
+	for(let i=0;i<l;i++){
+      const index=Math.floor(Math.random*chars.length)
+		str+=chars[index]
+	}
+
+	return str;
 }
 
-// Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+// // Do not change the code below.
+// const l = prompt("Enter a number.");
+// alert(makeid(l));
